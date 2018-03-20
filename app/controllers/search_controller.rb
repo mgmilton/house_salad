ef5c3a94748c4c39df2769801d56ce8c34187029
mgmilton <matt.milton@gmail.com>
2018-03-20 11:23:46 -0600

    adds house member ruby object

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,10 @@
 class SearchController < ApplicationController
   def index
-    @conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members/house/CO/current.json", :headers => {'X-API-Key' => ENV["propublica_key"]})
-    @house_members = []
+    state = params["state"]
+    conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members/house/#{state}/current.json", :headers => {'X-API-Key' => ENV["propublica_key"]})
+    response = conn.get
+    @house_members = JSON.parse(response.body)["results"].map do |raw_member|
+      HouseMember.new(raw_member)
+    end
   end
-end
-
-
-
-class HouseMember
-  attr_reader :name, :role, :party, :district
 end
--- spec/features/user_can_search_for_members_of_the_house_by_state_spec.rb
+++ spec/features/user_can_search_for_members_of_the_house_by_state_spec.rb
@@ -13,7 +13,7 @@ feature "User can search for members of the house by state" do
       expect(current_path).to eq("/search")
       expect(current_url).to include("state=CO")
       # And I should see a message "7 Results"
-      expect(page).to have_content("7 Results")
+      expect(page).to have_content("7 results")
       # And I should see a list of 7 the members of the house for Colorado
       expect(page).to have_css(".member", count: 7)
       # And they should be ordered by seniority from most to least
