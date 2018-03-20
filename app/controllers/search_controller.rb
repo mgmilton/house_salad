7b7d600eceb823bbc24862438ea56ef11bc0fe9f
mgmilton <matt.milton@gmail.com>
2018-03-20 11:15:37 -0600

    adds api request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,12 @@
 class SearchController < ApplicationController
   def index
+    @conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members/house/CO/current.json", :headers => {'X-API-Key' => ENV["propublica_key"]})
+    @house_members = []
   end
+end
+
+
+
+class HouseMember
+  attr_reader :name, :role, :party, :district
 end
