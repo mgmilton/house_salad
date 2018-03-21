644d63b1b7ba829e43087ffd9623883160671fb0
mgmilton <matt.milton@gmail.com>
2018-03-21 09:46:56 -0600

    refactors controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,22 @@
 class SearchController < ApplicationController
   def index
+    @house_members = PropublicaMembersByState.new(params[:state]).members
+
+
+
     state = params["state"]
     conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members/house/#{state}/current.json", :headers => {'X-API-Key' => ENV["propublica_key"]})
     response = conn.get
     @house_members = JSON.parse(response.body, symbolize_names: true)[:results].map do |raw_member|
       HouseMember.new(raw_member)
     end
+  end
+end
+
+class PropublicaMembersByState
+  def initialize(state)
+  end
+
+  def members
   end
 end
