ebb461481435671ee2d8378996c4f5c74acf4a13
mgmilton <matt.milton@gmail.com>
2018-03-21 11:42:50 -0600

    refactors models

--- app/services/propublica_service_member_search.rb
+++ app/services/propublica_service_member_search.rb
@@ -4,11 +4,16 @@ class PropublicaServiceMemberSearch
   end
 
   def run
-    conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members/house/#{state}/current.json", :headers => {'X-API-Key' => ENV["propublica_key"]})
-    response = conn.get
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
     attr_reader :state
+    def response
+      conn.get
+    end
+
+    def conn
+      Faraday.new(:url => "https://api.propublica.org/congress/v1/members/house/#{state}/current.json", :headers => {'X-API-Key' => ENV["propublica_key"]})
+    end
 end
