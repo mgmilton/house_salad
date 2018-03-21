e60b239309f09b17b106f8bbb327b46f530af3fd
mgmilton <matt.milton@gmail.com>
2018-03-21 11:07:38 -0600

    adds propublica service member search

--- app/services/propublica_members_by_state.rb
+++ app/services/propublica_members_by_state.rb
@@ -4,9 +4,7 @@ class PropublicaMembersByState
   end
 
   def members
-    conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members/house/#{state}/current.json", :headers => {'X-API-Key' => ENV["propublica_key"]})
-    response = conn.get
-    @house_members = JSON.parse(response.body, symbolize_names: true)[:results].map do |raw_member|
+    PropublicaServiceMemberSearch.new(state).run[:results].map do |raw_member|
       HouseMember.new(raw_member)
     end
   end
